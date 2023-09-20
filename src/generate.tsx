7daeeee2e63eef8cf269e284e869be12dd9ec089
Pierre-Yves Lebecq <pylebecq@users.noreply.github.com>
2023-09-20 12:18:52 +0200

    fix: Preserve previously used type in generate command

--- src/generate.tsx
+++ src/generate.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 export default function Command() {
   const [uuid, setUUID] = useState("");
-  const [type, setType] = useCachedState<string | undefined>("last-used-object-type", undefined);
+  const [type, setType] = useCachedState("last-used-object-type", "");
 
   const types = Array.from(TYPES_MAP, ([number, value]) => ({ number, value })).sort((a, b) =>
     a.value > b.value ? 1 : b.value > a.value ? -1 : 0
@@ -20,7 +20,7 @@ export default function Command() {
       globalIdFromUUID = create(type as ObjectType, uuid).toString();
     }
   } catch (err) {
-    uuidError = new Error('Wrong UUID format');
+    uuidError = new Error("Wrong UUID format");
   }
 
   function handleUUIDChanged(newValue: string) {
@@ -43,20 +43,29 @@ export default function Command() {
       <Form
         actions={
           <ActionPanel>
-            <Action
-              title="Copy GlobalId"
-              onAction={handleCopyGlobalId}
-            />
+            <Action title="Copy GlobalId" onAction={handleCopyGlobalId} />
           </ActionPanel>
         }
       >
-        <Form.Dropdown id="type" title="Object Type" placeholder="Choose a Type" onChange={handleTypeChanged}>
+        <Form.Dropdown
+          id="type"
+          title="Object Type"
+          placeholder="Choose a Type"
+          value={type}
+          onChange={handleTypeChanged}
+        >
           {types.map(({ number, value }) => {
             return <Form.Dropdown.Item key={number} value={value} title={`${value} (${number.toString()})`} />;
           })}
         </Form.Dropdown>
 
-        <Form.TextField id="uuid" title="Object ID" placeholder="56717c31-9b5a-4678-8c77-e7a62fde350c" error={uuidError?.message} onChange={handleUUIDChanged} />
+        <Form.TextField
+          id="uuid"
+          title="Object ID"
+          placeholder="56717c31-9b5a-4678-8c77-e7a62fde350c"
+          error={uuidError?.message}
+          onChange={handleUUIDChanged}
+        />
 
         <Form.Description title="Global ID" text={globalIdFromUUID || "-"} />
       </Form>
