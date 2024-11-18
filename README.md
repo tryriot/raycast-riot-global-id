86a5344bca8704ed72a32d7498a2ada406a38bbe
Reda <20306749+redac@users.noreply.github.com>
2024-11-18 11:57:14 +0100

    chore: update dependencies and switch to pnpm

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Introspect and generate Global ID retrieved from the Riot platform.
 ## Install the extension
 
 1. Checkout this repository
-2. `npm install && npm run build:local`
+2. `pnpm install && pnpm run build:local`
 3. Open Raycast > "Import extension" and select the `dist` directory inside the project folder.
 
 ## Contribute
 
-Run `npm run dev` to start Raycast development mode.
+Run `pnpm run dev` to start Raycast development mode.
 It will install the extension in your raycast and provide hot reload, detailed stack traces, etc.
---
+++ eslint.config.cjs
@@ -0,0 +1,31 @@
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const tsParser = require("@typescript-eslint/parser");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = [
+    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+        },
+    },
+];
